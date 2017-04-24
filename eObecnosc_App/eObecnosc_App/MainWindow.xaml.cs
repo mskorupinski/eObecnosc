332e9f1d0fcc5783cbb714ba8b1e1ac507a00579
Patryk <patryk-smol@wp.pl>
2017-04-24 15:01:07 +0200

    Dodano ekran logowania

--- eObecnosc_App/eObecnosc_App/MainWindow.xaml.cs
+++ eObecnosc_App/eObecnosc_App/MainWindow.xaml.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using MahApps.Metro.Controls;
+using MySql.Data.MySqlClient;
 
 namespace eObecnosc_App
 {
@@ -25,26 +26,36 @@ namespace eObecnosc_App
         {
             InitializeComponent();
         }
+        private string Nazwa_Użytkownika = "dbo674533357@10.46.135.36";
+        private string Hasło_Użytkownika = "$Dallas'63";
+        private string Serwer = "db674533357.db.1and1.com";
+        private string Baza = "db674533357 ";
 
-        private void Przycisk_Wyswietl_Click(object sender, RoutedEventArgs e)
+        private void Przycisk_Zaloguj_Click(object sender, RoutedEventArgs e)
         {
-            Panel.Children.Clear();
+            try
+            {
+                MySqlConnection con = new MySqlConnection("server=" + Serwer + ";UID=" + Nazwa_Użytkownika + ";database=" + Baza + ";password=" + Hasło_Użytkownika);
+                con.Open();
+                String readCommand = "SELECT COUNT(*) WHERE Login = @param_val_1 AND Password = @param_val_2;";
+                MySqlCommand m = new MySqlCommand(readCommand);
+                m.Parameters.AddWithValue("@param_val_1", Login.Text);
+                m.Parameters.AddWithValue("@param_val_2", Haslo.Password);
+                int liczba_uzytkownikow = Convert.ToInt32(m.ExecuteScalar());
+                if (liczba_uzytkownikow == 1)
+                {
+                    Widok.Children.Clear();
+                    Widok.Children.Add(new Sterowanie());
+                }
+                
+                con.Close();
+                
+            }
+            catch (Exception b)
+            {
+                MessageBox.Show(b.Message, "Błąd");
 
-            Panel.Children.Add(UC_Obecnosc.Instancja);
-        }
-
-        private void Przycisk_Dodaj_Click(object sender, RoutedEventArgs e)
-        {
-            Panel.Children.Clear();
-
-            Panel.Children.Add(UC_Dodaj.Instancja);
-        }
-
-        private void Przycisk_Edytuj_Click(object sender, RoutedEventArgs e)
-        {
-            Panel.Children.Clear();
-
-            Panel.Children.Add(UC_Edytuj.Instancja);
+            }
         }
     }
 }
---
+++ eObecnosc_App/eObecnosc_App/Sterowanie.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace eObecnosc_App
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Sterowanie.xaml
+    /// </summary>
+    public partial class Sterowanie : UserControl
+    {
+        public Sterowanie()
+        {
+            InitializeComponent();
+        }
+        private void Przycisk_Wyswietl_Click(object sender, RoutedEventArgs e)
+        {
+            Panel.Children.Clear();
+
+            Panel.Children.Add(UC_Obecnosc.Instancja);
+        }
+
+        private void Przycisk_Dodaj_Click(object sender, RoutedEventArgs e)
+        {
+            Panel.Children.Clear();
+
+            Panel.Children.Add(UC_Dodaj.Instancja);
+        }
+
+        private void Przycisk_Edytuj_Click(object sender, RoutedEventArgs e)
+        {
+            Panel.Children.Clear();
+
+            Panel.Children.Add(UC_Edytuj.Instancja);
+        }
+
+      
+    }
+}
